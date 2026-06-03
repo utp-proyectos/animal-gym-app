2acce27f68409834c6ea3989115f47fbf0d83526
leo <leonardoelbaneado@gmail.com>
2026-06-03 17:33:53 -0500

    feat:Agregacion de la ruta y el componente contenedor

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,15 @@
 import App from '@/App'
+import { EmployeePage } from '@/features/employee/components/EmployeePage'
 import { createBrowserRouter } from 'react-router-dom'
 
 const router = createBrowserRouter([
 	{
 		path: '/',
 		element: <App />,
+	},
+	{
+		path: '/employees',
+		element: <EmployeePage />,
 	},
 ])
 
