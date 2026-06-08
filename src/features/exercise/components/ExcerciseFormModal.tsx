80a9930463e473159302fac64ae3dcc1a59ec135
Jhordan <calixto4211@gmail.com>
2026-06-07 21:23:13 -0500

    feat: Agregar validacion con zod y rhf ejercicios

--- src/features/exercise/components/ExcerciseFormModal.tsx
+++ src/features/exercise/components/ExcerciseFormModal.tsx
@@ -1,11 +1,48 @@
 import { Button, Input, Label, Modal, Surface, TextField } from '@heroui/react'
 import { UserPlus } from 'lucide-react'
+import { useForm, Controller } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import z from 'zod'
 
 interface Props {
 	onClose: () => void
 }
 
+const exerciseSchema = z.object({
+	name: z
+		.string({ message: 'El nombre es requerido' })
+		.min(1, 'El nombre es requerido')
+		.min(3, 'El nombre debe tener al menos 3 caracteres'),
+	description: z
+		.string({ message: 'La descripción es requerida' })
+		.min(1, 'La descripción es requerida'),
+	muscleGroup: z
+		.string({ message: 'El grupo muscular es requerido' })
+		.min(1, 'El grupo muscular es requerido'),
+	equipment: z
+		.string({ message: 'El tipo de equipo es requerido' })
+		.min(1, 'El tipo de equipo es requerido'),
+})
+
+export type ExerciseFormInput = z.input<typeof exerciseSchema>
+export type ExerciseFormOutput = z.output<typeof exerciseSchema>
+
 export function ExcerciseFormModal({ onClose }: Props) {
+	const { control, handleSubmit } = useForm<ExerciseFormInput, unknown, ExerciseFormOutput>({
+		resolver: zodResolver(exerciseSchema),
+		defaultValues: {
+			name: '',
+			description: '',
+			muscleGroup: '',
+			equipment: '',
+		},
+	})
+
+	const onSubmit = (data: ExerciseFormOutput) => {
+		console.log('Prueba de envio :v', data)
+		onClose()
+	}
+
 	return (
 		<Modal
 			defaultOpen
@@ -29,32 +66,71 @@ export function ExcerciseFormModal({ onClose }: Props) {
 
 						<Modal.Body className="p-6">
 							<Surface variant="default">
-								<form className="flex flex-col gap-4">
-									<TextField className="w-full" variant="secondary">
-										<Label>Nombre</Label>
-										<Input placeholder="Ej. Curl de bíceps" />
-									</TextField>
-									<TextField className="w-full" variant="secondary">
-										<Label>Descripción</Label>
-										<Input placeholder="Ej. Ejercicio de fuerza" />
-									</TextField>
-									<TextField className="w-full" variant="secondary">
-										<Label>Grupo muscular</Label>
-										<Input placeholder="Ej. Bíceps" />
-									</TextField>
-									<TextField className="w-full" variant="secondary">
-										<Label>Equipo</Label>
-										<Input placeholder="Ej. Mancuernas" />
-									</TextField>
+								<form
+									id="exercise-form-modal"
+									className="flex flex-col gap-4"
+									onSubmit={handleSubmit(onSubmit)}
+								>
+									{/* Campo: Nombre */}
+									<Controller
+										name="name"
+										control={control}
+										render={({ field, fieldState: { error } }) => (
+											<TextField className="w-full" variant="secondary" isInvalid={!!error}>
+												<Label>Nombre</Label>
+												<Input {...field} placeholder="Ej. Curl de bíceps" />
+												{error && <p className="text-xs text-danger mt-1">{error.message}</p>}
+											</TextField>
+										)}
+									/>
+
+									{/* Campo: Descripción */}
+									<Controller
+										name="description"
+										control={control}
+										render={({ field, fieldState: { error } }) => (
+											<TextField className="w-full" variant="secondary" isInvalid={!!error}>
+												<Label>Descripción</Label>
+												<Input {...field} placeholder="Ej. Ejercicio de fuerza" />
+												{error && <p className="text-xs text-danger mt-1">{error.message}</p>}
+											</TextField>
+										)}
+									/>
+
+									{/* Campo: Grupo Muscular */}
+									<Controller
+										name="muscleGroup"
+										control={control}
+										render={({ field, fieldState: { error } }) => (
+											<TextField className="w-full" variant="secondary" isInvalid={!!error}>
+												<Label>Grupo muscular</Label>
+												<Input {...field} placeholder="Ej. Bíceps" />
+												{error && <p className="text-xs text-danger mt-1">{error.message}</p>}
+											</TextField>
+										)}
+									/>
+
+									{/* Campo: Equipo */}
+									<Controller
+										name="equipment"
+										control={control}
+										render={({ field, fieldState: { error } }) => (
+											<TextField className="w-full" variant="secondary" isInvalid={!!error}>
+												<Label>Equipo</Label>
+												<Input {...field} placeholder="Ej. Mancuernas" />
+												{error && <p className="text-xs text-danger mt-1">{error.message}</p>}
+											</TextField>
+										)}
+									/>
 								</form>
 							</Surface>
 						</Modal.Body>
 
 						<Modal.Footer className="pt-4">
-							<Button type="reset" variant="secondary" slot="close">
+							<Button variant="secondary" slot="close">
 								Cancelar
 							</Button>
-							<Button type="submit" form="form-modal-s">
+							<Button type="submit" form="exercise-form-modal">
 								<UserPlus className="size-4" />
 								Guardar ejercicio
 							</Button>
