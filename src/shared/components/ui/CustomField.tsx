a7a323e5a10ef49a6eb13a89a429547fe4e070f2
aomine <aomine1745@gmail.com>
2026-06-10 14:05:06 -0500

    feat: agregar componente custom field

---
+++ src/shared/components/ui/CustomField.tsx
@@ -0,0 +1,20 @@
+import { FieldError, Label, TextField } from '@heroui/react'
+
+interface FileFieldProps {
+	label?: string
+	errorMessage?: string
+	children: React.ReactNode
+}
+
+const CustomField = ({ label, errorMessage, children }: FileFieldProps) => {
+	const isInvalid = !!errorMessage
+	return (
+		<TextField isInvalid={isInvalid}>
+			<Label>{label}</Label>
+			{children}
+			<FieldError>{errorMessage}</FieldError>
+		</TextField>
+	)
+}
+
+export default CustomField
