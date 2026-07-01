b543925c4c078e51c17925b248ff4dca3966e545
aomine <aomine1745@gmail.com>
2026-07-01 08:15:49 -0500

    test: probar fallo de ci

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,3 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom'
-import { QueryClientProvider } from '@tanstack/react-query'
-
-import { queryClient } from '@/lib/queryClient'
-import { router } from '@/router'
-
-import '@/index.css'
-import { Toast } from '@heroui/react'
-
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Toast.Provider placement="bottom end" />
